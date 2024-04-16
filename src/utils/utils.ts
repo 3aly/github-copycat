45c41440c802e8b2221f26fb72ae2a3ae7459f84
3aly <ali.hefny1998@gmail.com>
2024-04-16 16:33:07 +0200

    add user slicer util for fake pagination

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,1 +1,5 @@
-export {};
+import { User } from "@datatypes/types";
+
+export const usersSlicer = (users: User[], currentPage: number): User[] => {
+  return users.slice((currentPage - 1) * 5, (currentPage - 1) * 5 + 5);
+};
