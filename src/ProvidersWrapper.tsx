7313c607d8f000a41f78b92c2b21562f3633e217
3aly <ali.hefny1998@gmail.com>
2024-04-16 16:30:54 +0200

    theme provider

--- src/ProvidersWrapper.tsx
+++ src/ProvidersWrapper.tsx
@@ -2,6 +2,14 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { Provider } from "react-redux";
 import { ReactElement } from "react";
 import { store } from "@redux/store";
+import {
+  createTheme,
+  ThemeProvider,
+  StyledEngineProvider,
+} from "@mui/material/styles";
+import { createUseCache } from "tss-react/makeStyles";
+import { CacheProvider } from "@emotion/react";
+import { theme } from "@constants/theme";
 
 export function ProvidersWrapper({
   children,
@@ -10,12 +18,19 @@ export function ProvidersWrapper({
 }) {
   const queryClient = new QueryClient();
 
+  // Create RTL cache
+  // const cacheRtl = createUseCache({
+  //   key: "muirtl",
+  //   stylisPlugins: [require("stylis-plugin-rtl")()],
+  // });
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        {/* <Provider store={store}> */}
-        {children}
-        {/* </Provider> */}
+        {/* <CacheProvider value={cacheRtl}> */}
+        <StyledEngineProvider injectFirst>
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </StyledEngineProvider>
+        {/* </CacheProvider> */}
       </QueryClientProvider>
     </>
   );
