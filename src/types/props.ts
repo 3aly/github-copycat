1b3e1839fd84bba2f1fcaad2895bfc4b1349bacd
3aly <ali.hefny1998@gmail.com>
2024-04-16 16:33:15 +0200

    adding props

--- src/types/props.ts
+++ src/types/props.ts
@@ -5,3 +5,13 @@ export interface TaskType {}
 //   avatar_url: string;
 //   html_url: string;
 // }
+export interface UserCardProps {
+  login: string;
+  avatar_url: string;
+  html_url: string;
+}
+export interface PaginationProps {
+  total: number; // Total number of users
+  currentPage: number; // Current page number
+  onPageChange: (page: number) => void; // Function to handle page change
+}
